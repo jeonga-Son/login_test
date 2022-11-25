0f5cde2991fb87055a64cd193d82ea6656957470
jeonga <jjeong0106@gmail.com>
2022-11-26 00:18:00 +0900

    [feat] 로그인 성공시 응답 본문에도 accessToken 넣기

--- wantrip/src/main/java/com/ja/exam/wantrip/base/dto/RsData.java
+++ wantrip/src/main/java/com/ja/exam/wantrip/base/dto/RsData.java
@@ -12,6 +12,10 @@ public class RsData<T> {
     private String msg;
     private T data;
 
+    public static <T> RsData<T> of(String resultCode, String msg, T data) {
+        return new RsData<>(resultCode, msg, null);
+    }
+
     public static <T> RsData<T> of(String resultCode, String msg) {
         return new RsData<>(resultCode, msg, null);
     }
