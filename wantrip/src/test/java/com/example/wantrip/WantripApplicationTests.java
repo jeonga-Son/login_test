ef15f872d1256485b2d232e1b83a8ac85cc44222
jeonga <jjeong0106@gmail.com>
2022-11-25 13:38:08 +0900

    [feat] 프로젝트 시작

---
+++ wantrip/src/test/java/com/example/wantrip/WantripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wantrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WantripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
