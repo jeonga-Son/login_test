4c20d5270818511f86ce0e5e692e421a42fcc4a7
jeonga <jjeong0106@gmail.com>
2022-11-25 22:57:40 +0900

    [test] 존재하지 않는 회원인지 체크, 비번체크

--- wantrip/src/main/java/com/ja/exam/wantrip/app/member/controller/MemberController.java
+++ wantrip/src/main/java/com/ja/exam/wantrip/app/member/controller/MemberController.java
@@ -16,11 +16,24 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/member")
+@RequiredArgsConstructor
 public class MemberController {
+    private final MemberService memberService;
+    private final PasswordEncoder passwordEncoder;
 
     @PostMapping("/login")
     public ResponseEntity<String> login(@RequestBody LoginDto loginDto) {
         if (loginDto.isNotValid()) {
+            return new ResponseEntity<>(null, null, HttpStatus.BAD_REQUEST);
+        }
+
+        Member member = memberService.findByUsername(loginDto.getUsername()).orElse(null);
+
+        if (member == null) {
+            return new ResponseEntity<>(null, null, HttpStatus.BAD_REQUEST);
+        }
+
+        if (passwordEncoder.matches(loginDto.getPassword(), member.getPassword()) == false) {
             return new ResponseEntity<>(null, null, HttpStatus.BAD_REQUEST);
         }
 
