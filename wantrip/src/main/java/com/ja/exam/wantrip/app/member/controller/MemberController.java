91dbb0da12bede98e9703fd122b2fe086f95f506
jeonga <jjeong0106@gmail.com>
2022-11-25 21:44:59 +0900

    [feat] JWT 로그인을 위한 SecurityConfig 설정

--- wantrip/src/main/java/com/ja/exam/wantrip/app/member/controller/MemberController.java
+++ wantrip/src/main/java/com/ja/exam/wantrip/app/member/controller/MemberController.java
@@ -1,9 +1,22 @@
 package com.ja.exam.wantrip.app.member.controller;
 
-import org.springframework.stereotype.Controller;
+import lombok.Data;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/member")
 public class MemberController {
+    @PostMapping("/login")
+    public String login(@RequestBody LoginDto loginDto) {
+        return "username : %s, password : %s".formatted(loginDto.getUsername(), loginDto.getPassword());
+    }
+
+    @Data
+    public static class LoginDto {
+        private String username;
+        private String password;
+    }
 }
