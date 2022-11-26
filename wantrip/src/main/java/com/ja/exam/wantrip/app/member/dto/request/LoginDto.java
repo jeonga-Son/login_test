a94f6c99136b56444446882f76d45c83c7e56c1e
jeonga <jjeong0106@gmail.com>
2022-11-26 17:03:41 +0900

    [feat] 입력데이터 유효성 체크를 라이브러리로, @RestControllerAdvice 로 전역에러핸들러 구현

--- wantrip/src/main/java/com/ja/exam/wantrip/app/member/dto/request/LoginDto.java
+++ wantrip/src/main/java/com/ja/exam/wantrip/app/member/dto/request/LoginDto.java
@@ -2,12 +2,13 @@ package com.ja.exam.wantrip.app.member.dto.request;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+
 @Data
 public class LoginDto {
+    @NotEmpty(message = "username 을(를) 입력해주세요.")
     private String username;
-    private String password;
 
-    public boolean isNotValid() {
-        return username == null || password == null || username.trim().length() == 0 || password.trim().length() == 0;
-    }
+    @NotEmpty(message = "password 을(를) 입력해주세요.")
+    private String password;
 }
